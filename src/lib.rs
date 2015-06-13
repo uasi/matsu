d0fe3ee206d78fcb5c74fffd3e7f1611be872219
Tomoki Aonuma <uasi@uasi.jp>
2015-06-13 20:12:32 +0900

    Add Examples to module doc

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,20 @@
 //! [ANSI escape codes]: https://en.wikipedia.org/wiki/ANSI_escape_code
 //!
 //! Matsu stands for Manipulate Attributed, Terminal-capable Strings in UTF-8.
+//!
+//! # Examples
+//!
+//! ```
+//! use matsu::{Attribute, AttributedString};
+//!
+//! let mut s = AttributedString::new("bold and underlined");
+//! s.add_attr(Attribute::Bold, 0..4);
+//! s.add_attr(Attribute::Underline, 9..19);
+//!
+//! assert_eq!(s.to_string(), "\x1B[1mbold\x1B[22m and \x1B[4munderlined\x1B[24m");
+//! ```
+//!
+//! In a terminal, the string will be printed as: **bold** and <u>underlined</u>
 
 mod attribute;
 mod attributed_string;
